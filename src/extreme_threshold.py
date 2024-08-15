86463a4556955836d433f032a35d6938f6a36fca
Quan <quan.liu@mpimet.mpg.de>
2024-08-15 15:34:15 +0200

    bug

--- src/extreme_threshold.py
+++ src/extreme_threshold.py
@@ -3,17 +3,22 @@ import pandas as pd
 
 
 # %%
-def threshold(df:pd.Dataframe, column_name: str ="pc", threshold: int = 1.5, extreme_type: str ="pos") -> pd.DataFrame:
+def threshold(
+    df: pd.DataFrame,
+    column_name: str = "pc",
+    threshold: int = 1.5,
+    extreme_type: str = "pos",
+) -> pd.DataFrame:
     """
     Calculate the threshold for the anomaly data across multiple years.
-    
+
     Parameters:
     df (pd.DataFrame): Input dataframe with columns ['time', column_name].
     column_name (str): The name of the column to be used in the threshold calculation.
     threshold (float): The threshold value. Default is 1.5 standard deviation.
     type (str): The type of threshold. Default is 'pos'.
 
-    
+
     """
 
     # make the dayofyear the same for both the normal year and leap year
@@ -48,7 +53,9 @@ def threshold(df:pd.Dataframe, column_name: str ="pc", threshold: int = 1.5, ext
 # %%
 
 
-def construct_window(df: pd.DataFrame, column_name: str ="pc", window: int =7) -> pd.DataFrame:
+def construct_window(
+    df: pd.DataFrame, column_name: str = "pc", window: int = 7
+) -> pd.DataFrame:
     """
     Create a dataframe, with window/2 - 1 days before and after the day as the window.
 
@@ -76,46 +83,52 @@ def construct_window(df: pd.DataFrame, column_name: str ="pc", window: int =7) -
 
     return df_stack
 
-#%%
 
-def subtract_threshold(df: pd.DataFrame, threshold: pd.DataFrame, column_name: str ='pc') -> pd.DataFrame:
+# %%
+
+
+def subtract_threshold(
+    df: pd.DataFrame, threshold: pd.DataFrame, column_name: str = "pc"
+) -> pd.DataFrame:
     """
     Subtract the threshold from the column_name for each day-of-year in df.
-    
+
     Parameters:
     df (pd.DataFrame): Input dataframe with columns ['time', column_name].
     threshold (pd.DataFrame): Input dataframe with columns ['dayofyear', 'threshold'].
     column_name (str): The name of the column to be used in the threshold calculation.
-    
+
     Returns:
     pd.DataFrame: Dataframe with the threshold subtracted from the specified column, named as 'residual'.
     """
     # Make the dayofyear the same for both the normal year and leap year
-    times = pd.to_datetime(df['time'].values)
+    times = pd.to_datetime(df["time"].values)
 
     # Identify leap years
     is_leap_year = times.is_leap_year
 
     # Adjust dayofyear for dates from March 1st onward in leap years
-    adjusted_dayofyear = times.dayofyear - is_leap_year * ((times.month > 2).astype(int))
+    adjusted_dayofyear = times.dayofyear - is_leap_year * (
+        (times.month > 2).astype(int)
+    )
 
     # Add adjusted_dayofyear to the dataframe
-    df['adjusted_dayofyear'] = adjusted_dayofyear
+    df["adjusted_dayofyear"] = adjusted_dayofyear
 
     # Ensure the threshold dataframe has a 'dayofyear' column
-    if 'dayofyear' not in threshold.columns:
+    if "dayofyear" not in threshold.columns:
         raise ValueError("The threshold dataframe must have a 'dayofyear' column.")
 
     # Merge the threshold with the df on 'adjusted_dayofyear' and any other columns apart from column_name
-    left_on = [col for col in df.columns if (col != column_name) & (col != 'time')]
-    right_on =[col for col in threshold.columns if col != 'threshold']
+    left_on = [col for col in df.columns if (col != column_name) & (col != "time")]
+    right_on = [col for col in threshold.columns if col != "threshold"]
 
-    df = pd.merge(df, threshold, left_on=left_on, right_on=right_on, how='left')
+    df = pd.merge(df, threshold, left_on=left_on, right_on=right_on, how="left")
 
     # Subtract the threshold from the specified column
-    df['residual'] = df[column_name] - df['threshold']
+    df["residual"] = df[column_name] - df["threshold"]
 
     # Drop the 'adjusted_dayofyear' and 'dayofyear' columns
-    df = df.drop(columns=['adjusted_dayofyear', 'dayofyear'])
+    df = df.drop(columns=["adjusted_dayofyear", "dayofyear"])
 
     return df
