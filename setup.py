57db050e9111932433e5e2c908d89cfe52821632
Quan <quan.liu@mpimet.mpg.de>
2024-08-16 14:46:22 +0200

    version greater

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="eventextreme",
-    version="0.0.3",
+    version="0.2.1",
     packages=find_packages(),
     install_requires=[
         "pandas",
