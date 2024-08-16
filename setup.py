6a7d7435a0e6894b6361d78e61791e508898e0b9
Quan <quan.liu@mpimet.mpg.de>
2024-08-16 13:45:31 +0200

    full setup

--- setup.py
+++ setup.py
@@ -12,6 +12,13 @@ setup(
     author="Quan Liu",
     author_email="quan.liu@mpimet.mpg.de",
     description="A package for extracting extreme events",
-    license="MIT",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/liuquan18/EventExtreme",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     python_requires=">=3.10",
 )
