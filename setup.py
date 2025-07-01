54c024e10592dbabefb6d3b80b3e3f1a08da9bd8
Liu Quan <57739182+liuquan18@users.noreply.github.com>
2025-07-01 17:57:40 +0200

    Update setup.py
    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="eventextreme",
-    version="0.7.0",
+    version="1.0",
     packages=find_packages(),
     install_requires=[
         "pandas",
